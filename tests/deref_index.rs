500e79919a8c546b7c4661479552c4c62b1dfc62
Warlock <internalmike@gmail.com>
2021-07-12 21:58:28 +0300

    delete dead parts, update copyrights, add $crate

--- tests/deref_index.rs
+++ tests/deref_index.rs
@@ -1,14 +1,14 @@
-/*
-Copyright ⓒ 2015 macro-attr contributors.
+// Copyright (c) 2015 macro-attr contributors.
+// Copyright (c) 2021 Warlock <internalmike@gmail.com>
+//
+// Licensed under the MIT license (see LICENSE or <http://opensource.org
+// /licenses/MIT>) or the Apache License, Version 2.0 (see LICENSE of
+// <http://www.apache.org/licenses/LICENSE-2.0>), at your option. All
+// files in the project carrying such notice may not be copied, modified,
+// or distributed except according to those terms.
 
-Licensed under the MIT license (see LICENSE or <http://opensource.org
-/licenses/MIT>) or the Apache License, Version 2.0 (see LICENSE of
-<http://www.apache.org/licenses/LICENSE-2.0>), at your option. All
-files in the project carrying such notice may not be copied, modified,
-or distributed except according to those terms.
-*/
-#[macro_use] extern crate macro_attr;
-#[macro_use] extern crate newtype_derive;
+#[macro_use] extern crate macro_attr_2018;
+#[macro_use] extern crate newtype_derive_2018;
 
 macro_attr! {
     #[derive(Clone, Eq, PartialEq, Debug,
