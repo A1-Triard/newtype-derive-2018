cf047a587c3844b9779f4541757129a5d13991fb
Warlock <internalmike@gmail.com>
2024-02-28 08:25:00 +0300

    ul

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@
 
 #![deny(warnings)]
 #![doc(test(attr(deny(warnings))))]
+#![doc(test(attr(allow(unknown_lints))))]
 #![doc(test(attr(allow(dead_code))))]
 #![doc(test(attr(allow(unused_variables))))]
 #![doc(test(attr(allow(unused_macros))))]
